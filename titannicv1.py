012d8242596c909a8b2a96031981d29e1ff4b809
ptopping <ptopping@gmail.com>
2018-11-28 17:54:37 -0700

    Delete titannicv1.py

--- titannicv1.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd 
-import matplotlib.pyplot as plt 
-from sklearn import linear_model
-
-train = pd.read_csv('c:\users\patrick\documents\kaggle\Titannic\Train.csv')
-test = pd.read_csv('c:\users\patrick\documents\kaggle\Titannic\Train.csv')
-
