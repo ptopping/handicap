5c50e96521479d17200cbed4508803b4c476cc9f
Patrick Topping <ptopping@gmail.com>
2019-06-13 11:18:52 -0600

    Update all to current version

--- handicap/NHLData.py
+++ handicap/NHLData.py
@@ -2,57 +2,65 @@
 import pandas as pd
 import statscrape as ss
 
-#Create URL Dictionary
-data_dict = {
-'teamsummary' : "http://www.nhl.com/stats/rest/grouped/team/basic/game/teamsummary",
-'franchisesummary' : "http://www.nhl.com/stats/rest/grouped/team/basic/game/franchisesummary",
-'powerplay' : "http://www.nhl.com/stats/rest/grouped/team/basic/game/powerplay",
-'penaltykill' : "http://www.nhl.com/stats/rest/grouped/team/basic/game/penaltykill",
-'penalties' : "http://www.nhl.com/stats/rest/grouped/team/basic/game/penalties",
-'realtime' : "http://www.nhl.com/stats/rest/grouped/team/basic/game/realtime",
-'faceoffsbystrength' : "http://www.nhl.com/stats/rest/grouped/team/basic/game/faceoffsbystrength",
-'shootout' : "http://www.nhl.com/stats/rest/grouped/team/shootout/game/shootout",
-'teamsummaryshooting' : "http://www.nhl.com/stats/rest/grouped/team/shooting/game/teamsummaryshooting",
-'teampercentages' : "http://www.nhl.com/stats/rest/grouped/team/shooting/game/teampercentages",
-'teamscoring' : "http://www.nhl.com/stats/rest/grouped/team/core/game/teamscoring",
-'faceoffsbyzone' : "http://www.nhl.com/stats/rest/grouped/team/core/game/faceoffsbyzone",
-'shottype' : "http://www.nhl.com/stats/rest/grouped/team/core/game/shottype"
-}
+#Addresses for NHL Historical Stats
+teamsummary = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=basic&isGame=true&reportName=teamsummary&cayenneExp=gameDate%3E=%222011-09-01%22%20and%20gameDate%3C=%222017-10-01%22%20and%20gameTypeId>=2"
+franchisesummary = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=basic&isGame=true&reportName=franchisesummary&cayenneExp=gameDate%3E=%222011-09-01%22%20and%20gameDate%3C=%222017-10-01%22%20and%20gameTypeId>=2"
+powerplay = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=basic&isGame=true&reportName=powerplay&cayenneExp=gameDate%3E=%222011-09-01%22%20and%20gameDate%3C=%222017-10-01%22%20and%20gameTypeId>=2"
+penaltykill = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=basic&isGame=true&reportName=penaltykill&cayenneExp=gameDate%3E=%222011-09-01%22%20and%20gameDate%3C=%222017-10-01%22%20and%20gameTypeId>=2"
+penalties = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=basic&isGame=true&reportName=penalties&cayenneExp=gameDate%3E=%222011-09-01%22%20and%20gameDate%3C=%222017-10-01%22%20and%20gameTypeId>=2"
+realtime = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=basic&isGame=true&reportName=realtime&cayenneExp=gameDate%3E=%222011-09-01%22%20and%20gameDate%3C=%222017-10-01%22%20and%20gameTypeId%3E=2"
+faceoffsbystrength = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=basic&isGame=true&reportName=faceoffsbystrength&cayenneExp=gameDate%3E=%222011-09-01%22%20and%20gameDate%3C=%222017-10-01%22%20and%20gameTypeId%3E=2"
+shootout = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=shootout&isGame=true&reportName=shootout&cayenneExp=gameDate%3E=%222011-09-01%22%20and%20gameDate%3C=%222017-10-01%22%20and%20gameTypeId%3E=2"
+teamgoalsbytype = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=basic&isGame=true&reportName=teamgoalsbytype&cayenneExp=gameDate%3E=%222011-09-01%22%20and%20gameDate%3C=%222017-10-01%22%20and%20gameTypeId>=2"
+teamdaysbetweengames = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=basic&isGame=true&reportName=teamdaysbetweengames&cayenneExp=gameDate%3E=%222011-09-01%22%20and%20gameDate%3C=%222017-10-01%22%20and%20gameTypeId%3E=2"
+teamsummaryshooting = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=shooting&isGame=true&reportName=teamsummaryshooting&cayenneExp=gameDate%3E=%222011-09-01%22%20and%20gameDate%3C=%222017-10-01%22%20and%20gameTypeId%3E=2"
+teampercentages = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=shooting&isGame=true&reportName=teampercentages&cayenneExp=gameDate%3E=%222011-09-01%22%20and%20gameDate%3C=%222017-10-01%22%20and%20gameTypeId%3E=2"
+teamscoring = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=core&isGame=true&reportName=teamscoring&cayenneExp=gameDate%3E=%222011-09-01%22%20and%20gameDate%3C=%222017-10-01%22%20and%20gameTypeId>=2"
+faceoffsbyzone = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=core&isGame=true&reportName=faceoffsbyzone&cayenneExp=gameDate%3E=%222011-09-01%22%20and%20gameDate%3C=%222017-10-01%22%20and%20gameTypeId%3E=2"
+shottype = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=core&isGame=true&reportName=shottype&cayenneExp=gameDate%3E=%222011-09-01%22%20and%20gameDate%3C=%222017-10-01%22%20and%20gameTypeId%3E=2"
 
-data_dict17 = {
-'teamsummary17' : "http://www.nhl.com/stats/rest/grouped/team/basic/game/teamsummary?cayenneExp=gameDate%3E=%222016-08-01T06:00:00.000Z%22",
-'franchisesummary17' : "http://www.nhl.com/stats/rest/grouped/team/basic/game/franchisesummary?cayenneExp=gameDate%3E=%222016-08-01T06:00:00.000Z%22",
-'powerplay17' : "http://www.nhl.com/stats/rest/grouped/team/basic/game/powerplay?cayenneExp=gameDate%3E=%222016-08-01T06:00:00.000Z%22",
-'penaltykill17' : "http://www.nhl.com/stats/rest/grouped/team/basic/game/penaltykill?cayenneExp=gameDate%3E=%222016-08-01T06:00:00.000Z%22",
-'penalties17' : "http://www.nhl.com/stats/rest/grouped/team/basic/game/penalties?cayenneExp=gameDate%3E=%222016-08-01T06:00:00.000Z%22",
-'realtime17' : "http://www.nhl.com/stats/rest/grouped/team/basic/game/realtime?cayenneExp=gameDate%3E=%222016-08-01T06:00:00.000Z%22",
-'faceoffsbystrength17' : "http://www.nhl.com/stats/rest/grouped/team/basic/game/faceoffsbystrength?cayenneExp=gameDate%3E=%222016-08-01T06:00:00.000Z%22",
-'shootout17' : "http://www.nhl.com/stats/rest/grouped/team/shootout/game/shootout?cayenneExp=gameDate%3E=%222016-08-01T06:00:00.000Z%22",
-'teamsummaryshooting17' : "http://www.nhl.com/stats/rest/grouped/team/shooting/game/teamsummaryshooting?cayenneExp=gameDate%3E=%222016-08-01T06:00:00.000Z%22",
-'teampercentages17' : "http://www.nhl.com/stats/rest/grouped/team/shooting/game/teampercentages?cayenneExp=gameDate%3E=%222016-08-01T06:00:00.000Z%22",
-'teamscoring17' : "http://www.nhl.com/stats/rest/grouped/team/core/game/teamscoring?cayenneExp=gameDate%3E=%222016-08-01T06:00:00.000Z%22",
-'faceoffsbyzone17' : "http://www.nhl.com/stats/rest/grouped/team/core/game/faceoffsbyzone?cayenneExp=gameDate%3E=%222016-08-01T06:00:00.000Z%22",
-'shottype17' : "http://www.nhl.com/stats/rest/grouped/team/core/game/shottype?cayenneExp=gameDate%3E=%222016-08-01T06:00:00.000Z%22",
-'schedule' : "https://statsapi.web.nhl.com/api/v1/schedule"
-}
+#stats = [teamsummary,franchisesummary,powerplay,penaltykill,penalties,realtime,faceoffsbystrength,shootout,teamgoalsbytype,teamdaysbetweengames,teamsummaryshooting,teampercentages,teamscoring,faceoffsbyzone,shottype]
 
-#Create list of Dictionary keys
-key_list = list(data_dict.keys())
-key_list17 = list(data_dict17.keys())
+#Addressesses for NHL Current Year Stats
+#teamsummary = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=basic&isGame=true&reportName=teamsummary&cayenneExp=gameDate%3E=%222017-09-01%22%20and%20gameDate%3C=%222018-10-01%22%20and%20gameTypeId>=2"
+#franchisesummary = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=basic&isGame=true&reportName=franchisesummary&cayenneExp=gameDate%3E=%222017-09-01%22%20and%20gameDate%3C=%222018-10-01%22%20and%20gameTypeId>=2"
+#powerplay = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=basic&isGame=true&reportName=powerplay&cayenneExp=gameDate%3E=%222017-09-01%22%20and%20gameDate%3C=%222018-10-01%22%20and%20gameTypeId>=2"
+#penaltykill = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=basic&isGame=true&reportName=penaltykill&cayenneExp=gameDate%3E=%222017-09-01%22%20and%20gameDate%3C=%222018-10-01%22%20and%20gameTypeId>=2"
+#penalties = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=basic&isGame=true&reportName=penalties&cayenneExp=gameDate%3E=%222017-09-01%22%20and%20gameDate%3C=%222018-10-01%22%20and%20gameTypeId>=2"
+#realtime = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=basic&isGame=true&reportName=realtime&cayenneExp=gameDate%3E=%222017-09-01%22%20and%20gameDate%3C=%222018-10-01%22%20and%20gameTypeId%3E=2"
+#faceoffsbystrength = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=basic&isGame=true&reportName=faceoffsbystrength&cayenneExp=gameDate%3E=%222017-09-01%22%20and%20gameDate%3C=%222018-10-01%22%20and%20gameTypeId%3E=2"
+#shootout = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=shootout&isGame=true&reportName=shootout&cayenneExp=gameDate%3E=%222017-09-01%22%20and%20gameDate%3C=%222018-10-01%22%20and%20gameTypeId%3E=2"
+#teamgoalsbytype = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=basic&isGame=true&reportName=teamgoalsbytype&cayenneExp=gameDate%3E=%222017-09-01%22%20and%20gameDate%3C=%222018-10-01%22%20and%20gameTypeId>=2"
+#teamdaysbetweengames = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=basic&isGame=true&reportName=teamdaysbetweengames&cayenneExp=gameDate%3E=%222017-09-01%22%20and%20gameDate%3C=%222018-10-01%22%20and%20gameTypeId%3E=2"
+#teamsummaryshooting = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=shooting&isGame=true&reportName=teamsummaryshooting&cayenneExp=gameDate%3E=%222017-09-01%22%20and%20gameDate%3C=%222018-10-01%22%20and%20gameTypeId%3E=2"
+#teampercentages = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=shooting&isGame=true&reportName=teampercentages&cayenneExp=gameDate%3E=%222017-09-01%22%20and%20gameDate%3C=%222018-10-01%22%20and%20gameTypeId%3E=2"
+#teamscoring = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=core&isGame=true&reportName=teamscoring&cayenneExp=gameDate%3E=%222017-09-01%22%20and%20gameDate%3C=%222018-10-01%22%20and%20gameTypeId>=2"
+#faceoffsbyzone = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=core&isGame=true&reportName=faceoffsbyzone&cayenneExp=gameDate%3E=%222017-09-01%22%20and%20gameDate%3C=%222018-10-01%22%20and%20gameTypeId%3E=2"
+#shottype = "http://www.nhl.com/stats/rest/team?isAggregate=false&reportType=core&isGame=true&reportName=shottype&cayenneExp=gameDate%3E=%222017-09-01%22%20and%20gameDate%3C=%222018-10-01%22%20and%20gameTypeId%3E=2"
 
+schedule = "https://statsapi.web.nhl.com/api/v1/schedule"
+
+#Create CSV of Data Tables --TO DO, Function to Iterate
 daily_sch = open('schedule.csv', 'w')
-daily_sch.write(ss.NHL_Stats(data_dict17.get('schedule')).schedule_df())
+daily_sch.write(ss.NHL_Stats(schedule).scheduleDF())
 daily_sch.close()
 
-#Remove schedule
-key_list17.remove('schedule')
+join1 = ss.df_join(ss.NHL_Stats(teamsummary).statsDF(),ss.NHL_Stats(franchisesummary).statsDF())
+join2 = ss.df_join(join1,ss.NHL_Stats(powerplay).statsDF())
+join3 = ss.df_join(join2,ss.NHL_Stats(penaltykill).statsDF())
+join4 = ss.df_join(join3,ss.NHL_Stats(penalties).statsDF())
+join5 = ss.df_join(join4,ss.NHL_Stats(realtime).statsDF())
+join6 = ss.df_join(join5,ss.NHL_Stats(faceoffsbystrength).statsDF())
+join7 = ss.df_join(join6,ss.NHL_Stats(teamgoalsbytype).statsDF())
+join8 = ss.df_join(join7,ss.NHL_Stats(teamdaysbetweengames).statsDF())
+join9 = ss.df_join(join8,ss.NHL_Stats(teamsummaryshooting).statsDF())
+join10 = ss.df_join(join9,ss.NHL_Stats(teampercentages).statsDF())
+join11 = ss.df_join(join10,ss.NHL_Stats(teamscoring).statsDF())
+join12 = ss.df_join(join11,ss.NHL_Stats(faceoffsbyzone).statsDF())
+join13 = ss.df_join(join12,ss.NHL_Stats(shottype).statsDF())
+join14 = ss.df_join(join13,ss.NHL_Stats(shootout).statsDF())
 
-for page in key_list:
-    stat_table = open('%s.csv' % page,'w')
-    stat_table.write(ss.NHL_Stats(data_dict.get(page)).statsDF())
-    stat_table.close()
+ts = open('NHLStats.csv', 'w')
+ts.write(pd.DataFrame.to_csv(join14.rename_axis('dfIndex'), encoding = 'utf-8'))
+ts.close()
 
-for page in key_list17:
-    stat_table = open('%s.csv' % page,'w')
-    stat_table.write(ss.NHL_Stats(data_dict.get(page)).statsDF())
-    stat_table.close()
