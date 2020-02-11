8219d89bc3a689d1b2ea19f3672226d230c16dbf
ptopping <ptopping@gmail.com>
2020-02-11 14:21:15 -0700

    Update statscrape.py

--- handicap/statscrape.py
+++ handicap/statscrape.py
@@ -13,19 +13,19 @@ class NHL_Stats(object):
 
 	Parameters
     
-    URL : str 
+    URL: str 
         URL for NHL stat API
 
     None
 	"""
-	def __init__(self,URL):
+	def __init__(self, URL):
 		self.URL = URL
 
 	def statsDF(self):
 		'''
         Connects to NHL Rest API and pulls data.
         
-        Returns : df
+        Returns: df
             Pandas DataFrame
 		'''
 		r = requests.get(self.URL)
@@ -54,9 +54,9 @@ class NHL_Stats(object):
 		df_home = pd.DataFrame(list(df2['home']))
 		df_away2 = pd.DataFrame(list(df_away['team']))
 		df_home2 = pd.DataFrame(list(df_home['team']))
-		df_fin = pd.DataFrame({'gameDate' : df1['gameDate'], 'visitor' : df_away2['name'], 'home' : df_home2['name']})
+		df_fin = pd.DataFrame({'gameDate': df1['gameDate'], 'visitor': df_away2['name'], 'home': df_home2['name']})
 		df_fin['gameDate'] = df_fin['gameDate'].apply(zulu_con).astype('datetime64[ns, MST]')
-		converted = pd.DataFrame.to_csv(df_fin.rename_axis('dfIndex'), encoding = 'utf-8')
+		converted = pd.DataFrame.to_csv(df_fin.rename_axis('dfIndex'), encoding='utf-8')
 		return converted
 
 class NFLStats(object):
@@ -65,16 +65,16 @@ class NFLStats(object):
 
 	Parameters
 
-	season_type : Str
+	season_type: Str
 		Must be one of three values: 'pre', 'reg', 'post' which is equivalent to Preseason, Regular Season and Post Season respectively
-	season : Int
+	season: Int
 		Calendar year of the NFL season, only years 2001 and later are available
-	week : Int
+	week: Int
 		Integer with a range of 1-17
-    data : List-like
+    data: List-like
         Empty list filled by class methods
 	"""
-	def __init__(self,season_type,season,week):
+	def __init__(self, season_type, season, week):
 		self.season_type = season_type
 		self.season = season
 		self.week = week
@@ -92,38 +92,35 @@ class NFLStats(object):
 		"""
 
 		r = requests.get(self.url)
-		soup = BeautifulSoup(r.text,'html.parser')
+		soup = BeautifulSoup(r.text, 'html.parser')
 		games = soup.select('.gc-btn')
 
 		for g in games:
 			url = g.get('href')
 			r = requests.get(url)
-			soup = BeautifulSoup(r.text,'html.parser')
-			m = re.search('\{(.*?})*',soup.find('script',string=re.compile('INITIAL')).string)
+			soup = BeautifulSoup(r.text, 'html.parser')
+			m = re.search('\{(.*?})*', soup.find('script',string=re.compile('INITIAL')).string)
 
 			self.data.append(json.loads(m.group(0))['instance'])
 			
-	def prettify(self, toplevel):
+	def prettify(self, df):
         '''
-        Series of repeated functions create method reuses.  Each row will have unique id
+        Series of repeated functions to trim and organize dataframes
         
         Parameters
         
-        toplevel : str
-            Select the highest level to strip of nested lists and organize.  Can contain 'game', 'gameDetails'
-
-        Returns : df
+        toplevel: df
             Pandas dataframe
+
+        Returns: df
+            Pandas prettified dataframe
             
-        Raises
+        Raises:
             ValueError: If toplevel not of specified value
         '''
-        if toplevel in ('game', 'gameDetails'):
-            df = pd.io.json.json_normalize(data=[d.get(toplevel) for d in self.data], sep='_')
-            
-            # Find and remove columns which are lists or NULL.  Either types present issues for import into SQL
+		# Find and remove columns which are lists or NULL.  Either types present issues for import into SQL
             cols = [c for c in df.columns if not df[c].apply(isinstance, args=[list]).any()]
-			df = df[cols].dropna(how='all', axis=1)
+			df = df[cols].dropna(how='all', axis = 1)
 
             # Oracle doesn't have a BOOL dtype so booleans must be converted
             bools = df.select_dtypes(include='bool').columns
@@ -146,7 +143,7 @@ class NFLStats(object):
 
 		Parameters
         
-        frame : str
+        frame: str
 			select which dataframe to return.  Choose from 'game', 'gameDetails', 'scoringSummaries', 'drives', 'plays', 'playStats', 'gamePlayerStats',
 			'teamStats',  standings
 		
@@ -158,16 +155,17 @@ class NFLStats(object):
 
         # Must run self.scrape first to populate self.data
 		if frame in ['gameDetails', 'scoringSummaries', 'drives', 'plays', 'playStats']:
-			df = self.prettify(toplevel='gameDetails')
+			df = pd.io.json.json_normalize(data=[d.get('gameDetails') for d in self.data], sep='_')
+			df = self.prettify(df)
 
 			if frame in ['scoringSummaries', 'drives']:
                 # scoringSummaries and drives are both lists within gameDetails.  Must iterate through gameDetails to extract
-                df2 = pd.DataFrame()
+                	df2 = pd.DataFrame()
 				for i in range(len(df)):
 					df3 = pd.io.json.json_normalize(data=df[frame][i], sep='_')
                     
                     # Which game the drive belongs to                  
-					df3['id'] = df.loc[i,'id'] 
+					df3['id'] = df.loc[i, 'id'] 
 					df2 = df2.append(df3, sort=False)
 				
                 # For consistency rename to df                
@@ -192,7 +190,7 @@ class NFLStats(object):
 					df3 = pd.io.json.json_normalize(data=df['plays'][i], sep='_')
                     
                     # Which game the play belongs to
-					df3['id'] = df.loc[i,'id'] 
+					df3['id'] = df.loc[i, 'id'] 
 					df2 = df2.append(df3, sort=False)
 				
                 df2.reset_index(drop=True, inplace=True)
@@ -212,13 +210,13 @@ class NFLStats(object):
 							df5 = pd.io.json.json_normalize(data=df2['playStats'][i], sep='_')
 							
                             # # Which game and which play the playStat belongs to                            
-                            df5['id'] = df2.loc[i,'id']
-							df5['playId'] = df2.loc[i,'playId']
+                            df5['id'] = df2.loc[i, 'id']
+							df5['playId'] = df2.loc[i, 'playId']
 							df4 = df4.append(df5, sort=False)
                             
 						except AttributeError:
 #                           The opening play of each game is an empty value which will throw an AttributeError                             
-							df5 = pd.DataFrame({'id':{i:df2.loc[i,'id']}, 'playId':{i:df2.loc[i,'playId']}})
+							df5 = pd.DataFrame({'id':{i:df2.loc[i,'id']}, 'playId':{i:df2.loc[i, 'playId']}})
 							df4 = df4.append(df5, sort=False)
 					
                     # For consistency rename to df 
@@ -243,7 +241,7 @@ class NFLStats(object):
 		# Game DataFrame
 		elif frame == 'game':
 			df = pd.io.json.json_normalize(data=[d.get('game') for d in self.data], sep='_')
-			df.reset_index(drop=True,inplace=True)
+			df.reset_index(drop=True, inplace=True)
             
             # Find and remove columns which are lists or NULL.  Either types present issues for import into SQL
 			cols = [c for c in df.columns if not df[c].apply(isinstance, args=[list]).any()]
@@ -260,8 +258,8 @@ class NFLStats(object):
 		elif frame == 'gamePlayerStats':
 			df = pd.DataFrame()
 			for d in range(len(self.data)):
-				df = df.append(pd.io.json.json_normalize(data=self.data[d].get('gamePlayerStats'), sep='_'),sort=False)
-			df.reset_index(drop=True,inplace=True)
+				df = df.append(pd.io.json.json_normalize(data=self.data[d].get('gamePlayerStats'), sep='_'), sort=False)
+			df.reset_index(drop=True, inplace=True)
 			bools = df.select_dtypes(include='bool').columns
 			for b in bools:
 				df[b] = df[b].astype(int)
@@ -278,7 +276,7 @@ class NFLStats(object):
 			df3 = pd.io.json.json_normalize(df.awayTeamStats.apply(pd.Series)[0], sep='_')
 			df = df2.join(df3, lsuffix='_home', rsuffix='_away')
 			df = df.join(ids)
-			df.reset_index(drop=True,inplace=True)
+			df.reset_index(drop=True, inplace=True)
 			bools = df.select_dtypes(include='bool').columns
 			for b in bools:
 				df[b] = df[b].astype(int)
@@ -294,11 +292,11 @@ class NFLStats(object):
 			cols = [c for c in df.columns if not df[c].apply(isinstance, args=[list]).any()]
 			df2 = pd.DataFrame()
 			for i in range(len(df)):
-				df3 = pd.DataFrame(df.loc[i,'teamRecords'])
-				df3['week_id'] = df.loc[i,'week_id']
+				df3 = pd.DataFrame(df.loc[i, 'teamRecords'])
+				df3['week_id'] = df.loc[i, 'week_id']
 				df2 = df2.append(df3, sort=False)
 			df = df[cols].merge(df2, how='left', left_on='week_id', right_on='week_id')
-			df.reset_index(drop=True,inplace=True) 
+			df.reset_index(drop=True, inplace=True) 
 			bools = df.select_dtypes(include='bool').columns
 			for b in bools:
 				df[b] = df[b].astype(int)
@@ -311,13 +309,13 @@ class NFLStats(object):
 			return ValueError
 
 #Convert DataFrame to csv for export
-def create_csv(frame,name):
+def create_csv(frame, name):
 	'''
 	Input = Pandas DataFrame
 	Output = csv text
 	'''
-	converted = frame.to_csv(index=False,sep='|',encoding='utf-8',line_terminator='///x')
-	stat_table = open ('d:\\depthandtaxes\\{}.csv'.format(name),'w')
+	converted = frame.to_csv(index=False,sep='|', encoding='utf-8', line_terminator='///x')
+	stat_table = open('d:\\depthandtaxes\\{}.csv'.format(name),'w')
 	stat_table.write(converted)
 	stat_table
 
@@ -337,5 +335,5 @@ def df_join(df1, df2):
 	for obj in listcols1:
 		if obj in listcols2:
 			cols.append(obj)
-	merged = pd.merge(df1, df2, on = cols, how = 'left')
+	merged = pd.merge(df1, df2, on=cols, how='left')
 	return merged
